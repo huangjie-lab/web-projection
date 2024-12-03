e0bab64410a7e9964f297ec581195aa103e10bdc
huangjie <huangjie6@codemao.cn>
2024-12-03 14:13:50 +0800

    fix: 修复部分eslint，还有待加强

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,9 +4,9 @@ module.exports = {
   // 行尾是否使用分号，默认为true
   semi: true,
   // 字符串是否使用单引号，默认为false，使用双引号
-  singleQuote: true,
+  singleQuote: false,
   // 一个tab代表几个空格数，默认为2
   tabWidth: 2,
   // 是否使用尾逗号，有三个可选值"<none|es5|all>"
-  trailingComma: 'none'
+  trailingComma: "none"
 };

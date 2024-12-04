d3fdad46dcb98cb07b236d5a5c30a53f4e6b0e47
huangjie <huangjie6@codemao.cn>
2024-12-04 11:16:59 +0800

    🐛 fix: 删除不必要的log打印

--- .husky/commit-msg-emoji.js
+++ .husky/commit-msg-emoji.js
@@ -7,21 +7,14 @@ const typeToEmojiMap = {
 };
 
 const COMMIT_EDITING_FILEPATH = process.argv.at(-1);
-console.log(process.argv, 'process.argv');
-
-console.log(COMMIT_EDITING_FILEPATH, 'COMMIT_EDITING_FILEPATH');
 
 const inputMsg = readFileSync(COMMIT_EDITING_FILEPATH, 'utf8');
 
 writeFileSync(COMMIT_EDITING_FILEPATH, transform(inputMsg, typeToEmojiMap), 'utf8');
 
 function transform(inputMsg, typeToEmojiMap) {
-  console.log(inputMsg, 'inputMsg');
-
   const [type, emoji] =
     Object.entries(typeToEmojiMap).find(([type]) => inputMsg.startsWith(type)) ?? [];
-
-  console.log(type, 'type');
 
   if (type) {
     return inputMsg.replace(new RegExp(`^${type}`), `${emoji} $&`);

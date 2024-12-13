43c65a2cf50b991e4bc7ddad3e7ba7605c84c371
huangjie <huangjie6@codemao.cn>
2024-12-13 23:35:08 +0800

    ✨ feat: 修改下提交代码检查规范

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from './index.module.less';
 
 const About: React.FC = () => {
-  const a = 1;
-  a = 2;
   return <div className={styles.about}>about</div>;
 };
 export default About;

ee9d4fb36f1328d0014f7aed952d00c81a3fdc9a
huangjie <huangjie6@codemao.cn>
2024-12-13 23:32:50 +0800

    ✨ feat: 测试eslint错误检查

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './index.module.less';
 
 const About: React.FC = () => {
+  const a = 1;
+  a = 2;
   return <div className={styles.about}>about</div>;
 };
 export default About;
